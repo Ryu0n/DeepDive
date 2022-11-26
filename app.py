7d7d430eadfa809a11e1ea8a63324675e8406ec7
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2022-11-26 22:41:56 +0900

    :hammer: [FIX] app.py

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI
 from dto import Sentences
-from dataloader import dataloader
+from transformers import BertForSequenceClassification, BertTokenizer
 
 
 app = FastAPI()
 
 
-def input_sentences():
-    return dataloader(is_train=True, batch_size=16)
+@app.post('/analysis')
+def analysis_semtiment(sentences: Sentences):
+    load_model = BertForSequenceClassification.from_pretrained('klue/bert-base')
+    tokenizer = BertTokenizer.from_pretrained('klue/bert-base')
+    tokenized_inputs = tokenizer.encode_plus(sentences,
+                                             max_length='max_length',
+                                             return_tensors='pt')
 
-
-def save_senteces(sentences: Sentences):
-
-    pass
-
-
-@app.post("/tokenize")
-def tokenize_server(sentences: Sentences):
     pass
