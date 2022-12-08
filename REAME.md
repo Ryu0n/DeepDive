bf1c777eeb99a4174028435bf083ff38724333b2
Ryu0n <hsung951027@gmail.com>
2022-12-08 10:54:28 +0900

    :rainbow: First Commit

---
+++ REAME.md
@@ -0,0 +1,64 @@
+# Motivation
+```
+NOTE  
+한국어는 교착어 특성상 여러 품사가 띄어쓰기 구분 없이 어절 단위로 합쳐져 있기 때문에 세부적인 분석이 어렵습니다.
+반면, 영어는 띄어쓰기 단위로 품사가 구분되어 있기 때문에 ABSA와 같이 TokenClassification이 유리합니다.
+
+ex)
+영어 : .. (pancake, '명사') (is, '조사') ..
+한국어 : .. 팬케이크는 .. 
+```
+- 기존의 BERT 모델은 품사 정보를 반영하지 않았습니다.
+- 해당 모델은 ```BertTokenizerFast```로 분해된 token들에 ```kiwi```형태소 분석기의 품사를 태깅합니다.
+- 모델의 embedding 레이어에 각 토큰들의 품사 임베딩을 반영했습니다.  
+
+# Usage
+```
+git clone https://github.com/Ryu0nPrivateProject/POSBert.git
+```
+```python
+from custom_bert_tokenizer import tokenize
+from custom_bert import CustomBertForTokenClassification
+
+sentences = [
+    '나는 사과를 먹었다.',
+    '이번 이태원 대참사 진짜 장난 아니네..',
+    '너무 힘든데 게임하는건 좋아유'
+]
+
+inputs = tokenize(sentences=sentences)
+model = CustomBertForTokenClassification.from_pretrained('klue/bert-base', num_labels=4)
+outputs = model(**inputs)
+
+print(outputs)
+
+"""
+TokenClassifierOutput(loss=None, logits=tensor([[[-0.2775, -0.2969, -0.0903, -0.0018],
+         [-0.0091,  0.2871, -0.7079,  0.2197],
+         [ 0.4855,  0.3676, -0.2845, -0.3408],
+         ...,
+         [-0.2271, -0.2526, -0.2464,  0.0590],
+         [ 0.0728, -0.2573, -0.0605, -0.0643],
+         [-0.0497, -0.2890, -0.2162, -0.1672]],
+
+        [[-0.8450,  0.0222, -0.2058,  0.2605],
+         [-0.3437, -0.1106, -1.0901, -0.4831],
+         [-0.4604, -0.0313, -0.1097,  0.1768],
+         ...,
+         [-0.8348, -0.4279,  0.1969, -0.4718],
+         [-0.9340, -0.3948,  0.2108, -0.5739],
+         [-0.6525, -0.4227, -0.0456, -0.0488]],
+
+        [[-1.1676,  0.0696, -0.0715,  0.4199],
+         [-0.8934,  0.2599, -1.0831,  0.6113],
+         [-0.8213,  0.1867, -0.2176, -0.0370],
+         ...,
+         [-0.4724, -0.1521,  0.3033,  0.0240],
+         [-0.3632, -0.2972,  0.2225, -0.0881],
+         [-0.6073, -0.2911,  0.4057,  0.0735]]], grad_fn=<ViewBackward0>), hidden_states=None, attentions=None)
+"""
+```
+
+# Future Work
+- 일부 코드 리팩토링
+- ELECTRA 버전 구현
---
+++ custom_bert_tokenizer.py
@@ -0,0 +1,83 @@
+import torch
+
+from utils import load_kiwi_pos_dict
+from typing import List, Dict
+from transformers import BertTokenizerFast
+from kiwipiepy import Kiwi
+
+kiwi_tokenizer = Kiwi()
+bert_tokenizer = BertTokenizerFast.from_pretrained('klue/bert-base')
+
+
+def __tokenize_by_kiwi(sentences: List[str]) -> List[List[Dict]]:
+    kiwi_sentences_tokens = list(kiwi_tokenizer.tokenize(sentences))
+    sentences_pos_spans = list()
+    for kiwi_tokens in kiwi_sentences_tokens:
+        pos_spans = list()
+        for token in kiwi_tokens:
+            pos_spans.append(
+                {
+                    'span': [token.start, token.end],
+                    'pos': token.tag
+                }
+            )
+        sentences_pos_spans.append(pos_spans.copy())
+    return sentences_pos_spans
+
+
+def __tokenize_by_bert(sentences: List[str]):
+    inputs = bert_tokenizer(sentences,
+                            return_offsets_mapping=True,
+                            return_tensors='pt',
+                            padding='max_length')
+    return inputs
+
+
+def tokenize(sentences: List[str]):
+    """
+    Custom Tokenization
+    """
+    """
+    KIWI TOKENIZER
+    """
+    sentences_pos_spans = __tokenize_by_kiwi(sentences)
+
+    """
+    BERT TOKENIZER
+    """
+    inputs = __tokenize_by_bert(sentences)
+    # print(inputs)
+
+    """
+    POS TAGGING TO BERT TOKENS
+    """
+    sentences_input_ids = inputs.get('input_ids')
+    token_to_id_vocab = bert_tokenizer.get_vocab()
+    id_to_token_vocab = {v: k for k, v in token_to_id_vocab.items()}
+    sentences_offsets = inputs.get('offset_mapping')
+    special_tokens = bert_tokenizer.special_tokens_map.values()
+    kiwi_pos_dict = load_kiwi_pos_dict()
+    sentences_pos_tags = list()
+    for sentence_index, (input_ids, offsets) in enumerate(zip(sentences_input_ids, sentences_offsets)):
+        input_ids = input_ids.tolist()
+        offsets = offsets.tolist()
+        tokens = [(id_to_token_vocab.get(id), offset) for id, offset in zip(input_ids, offsets)]
+        pos_tags = list()
+        for token in tokens:
+            if token[0] in special_tokens:
+                pos_tags.append(kiwi_pos_dict.get("NOTHING"))
+                continue
+            bert_token_start, bert_token_end = token[-1]
+            pos_spans = sentences_pos_spans[sentence_index]
+            for pos_span in pos_spans:
+                kiwi_start, kiwi_end = pos_span.get('span')
+                if kiwi_start <= bert_token_start and bert_token_end <= kiwi_end:
+                    pos_tags.append(
+                        kiwi_pos_dict.get(
+                            pos_span.get('pos')
+                        )
+                    )
+        sentences_pos_tags.append(pos_tags.copy())
+    sentences_pos_tags = torch.tensor(sentences_pos_tags, dtype=torch.int64)
+    inputs['pos_tag_ids'] = sentences_pos_tags
+    return inputs
