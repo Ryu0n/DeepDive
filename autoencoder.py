8e8f6f85075c9f7cd63ba1f64beb59d5334d1f76
Ryu0n <hsung951027@gmail.com>
2022-12-11 12:21:40 +0900

    :hammer: [FIX] fix autoencoder.py to accept vector dimension dynamically

--- autoencoder.py
+++ autoencoder.py
@@ -6,26 +6,36 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, vector_dim: int):
         super(AutoEncoder, self).__init__()
-        word_vector_dim = 300
+        self.vector_dim = vector_dim
+        self.compressed_dim = self.vector_dim // 3
+        compress_unit = (self.vector_dim - self.compressed_dim) // 4
         self.encoder = nn.Sequential(
-            nn.Linear(in_features=word_vector_dim, out_features=250),
+            nn.Linear(in_features=self.vector_dim,
+                      out_features=self.vector_dim - compress_unit),
             nn.LeakyReLU(),
-            nn.Linear(in_features=250, out_features=200),
+            nn.Linear(in_features=self.vector_dim - compress_unit,
+                      out_features=self.vector_dim - 2*compress_unit),
             nn.LeakyReLU(),
-            nn.Linear(in_features=200, out_features=150),
+            nn.Linear(in_features=self.vector_dim - 2*compress_unit,
+                      out_features=self.vector_dim - 3*compress_unit),
             nn.LeakyReLU(),
-            nn.Linear(in_features=150, out_features=100),
+            nn.Linear(in_features=self.vector_dim - 3*compress_unit,
+                      out_features=self.compressed_dim),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(in_features=100, out_features=150),
+            nn.Linear(in_features=self.compressed_dim,
+                      out_features=self.vector_dim - 3*compress_unit),
             nn.LeakyReLU(),
-            nn.Linear(in_features=150, out_features=200),
+            nn.Linear(in_features=self.vector_dim - 3*compress_unit,
+                      out_features=self.vector_dim - 2*compress_unit),
             nn.LeakyReLU(),
-            nn.Linear(in_features=200, out_features=250),
+            nn.Linear(in_features=self.vector_dim - 2*compress_unit,
+                      out_features=self.vector_dim - compress_unit),
             nn.LeakyReLU(),
-            nn.Linear(in_features=250, out_features=word_vector_dim)
+            nn.Linear(in_features=self.vector_dim - compress_unit,
+                      out_features=self.vector_dim)
         )
 
     def forward(self, x):
@@ -55,7 +65,7 @@ def dataloader(sentence_vectors: np.ndarray):
 
 
 def train_auto_encoder(sentence_vectors: np.ndarray, num_epochs=10, lr=0.005):
-    ae = AutoEncoder()
+    ae = AutoEncoder(sentence_vectors.shape[-1])
     train_loader = dataloader(sentence_vectors)
     optim = torch.optim.Adam(ae.parameters(), lr=lr)
     criterion = nn.MSELoss()
